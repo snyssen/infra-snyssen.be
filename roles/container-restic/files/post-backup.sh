7ee754decf56c60344c335a0e17b87ddae44e614
Simon Nyssen <simon.nyssen@mailfence.com>
2022-06-24 23:22:42 +0200

    [WIP] Adds pre- and post-backup scripts

---
+++ roles/container-restic/files/post-backup.sh
@@ -0,0 +1,1 @@
+curl --retry 3 https://hc-ping.com/${HEALTHCHECK_ID}/$? # FIXME: $? always returns 0 no matter the backup result
---
+++ roles/container-restic/files/pre-backup.sh
@@ -0,0 +1,1 @@
+curl --retry 3 https://hc-ping.com/${HEALTHCHECK_ID}/start
