eb64701434403b84d0783941136e85cf2cd22a1d
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-29 21:26:40 +0200

    Refactors vagrant related scripts

--- vagrant-update-vm-keys.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/sh
-ssh-keyscan snyssen.duckdns.org >>~/.ssh/known_hosts
-ssh-keyscan backup.sny >>~/.ssh/known_hosts
-ssh-copy-id vagrant@snyssen.duckdns.org
-ssh-copy-id vagrant@backup.sny
