f04975ca4f6549bbac0e147fd411919d9558f1d9
Simon Nyssen <dev@snyssen.be>
2023-02-09 18:01:52 +0100

    [WIP] wake and shutdown backup server during backup operations

--- roles/container_restic/files/local/post-backup.sh
+++ roles/container_restic/files/local/post-backup.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
 curl --retry 3 https://hc-ping.com/"${HEALTHCHECK_ID}"/"$1" # $1 is the status code returned by the restic command
+ssh -i /home/backup/.ssh/backup_rsa -o "StrictHostKeyChecking no" "backup@${BACKUP_SERVER_DOMAIN}" "sudo poweroff"
--- roles/container_restic/files/local/pre-backup.sh
+++ roles/container_restic/files/local/pre-backup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 curl --retry 3 https://hc-ping.com/"${HEALTHCHECK_ID}"/start
-awake "${BACKUP_SERVER_MAC_ADDRESS}" # TODO: Set as variable
-# Try to connect to metrics endpoint up to 10 times with a large timeout (2s) and check if HTTP 200 status is returned
+awake "${BACKUP_SERVER_MAC_ADDRESS}"
+# Try to connect to metrics endpoint up to 10 times with a large timeout (30s) and check if HTTP 200 status is returned
 # If command fails, echo the error and ping healthcheck
-(curl --retry 10 --connect-timeout 2 -s -D - "http://${BACKUP_SERVER_DOMAIN}:8000/metrics" -o /dev/null 2>/dev/null | head -n1 | grep 200) \
+(curl --retry 10 -m 30 -s -D - "http://${BACKUP_SERVER_DOMAIN}:8000/metrics" -o /dev/null 2>/dev/null | head -n1 | grep 200) \
     || (RESULT="$?" && echo "${RESULT}: Failed to start backup server" && curl --retry 3 https://hc-ping.com/"${HEALTHCHECK_ID}"/"${RESULT}")
