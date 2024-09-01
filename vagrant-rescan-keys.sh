4d2856c3c63b0a0d56bdfef126b8dbdb4e2b9d88
Simon Nyssen <dev@snyssen.be>
2024-09-01 11:46:12 +0200

    chore: remove vagrant stuff

--- vagrant-rescan-keys.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-# Builds a list of all the virtual machines hosts
-hosts=("snyssen.duckdns.org " "backup.sny")
-# Then scans the public key of each of those hosts, replacing the previously known one
-for host in "${hosts[@]}"; do
-	ssh-keyscan "${host}" >>~/.ssh/known_hosts
-done
