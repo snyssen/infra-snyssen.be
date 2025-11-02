99e4f77e9401b7535174a1247a88dac806b38eb5
snyssen <dev@snyssen.be>
2025-11-02 21:32:31 +0100

    feat(tf/hetzner): [WIP] add terraform for Hetzner cloud

--- flake.nix
+++ flake.nix
@@ -3,23 +3,27 @@
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nixpkgs-fmt
-          just
-          pre-commit
-          ansible
-          ansible-lint
-          glibcLocales # otherwise ansible cannot run
-          d2
-          grafana-alloy
-        ];
-      };
-    }
-  );
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixfmt
+            nixd
+            just
+            pre-commit
+            ansible
+            ansible-lint
+            glibcLocales # otherwise ansible cannot run
+            d2
+            grafana-alloy
+            terraform
+          ];
+        };
+      });
 }
